7e5416934f9f272ef7fb1b39039a2a6d00349de0
jhyer2 <jeremy.hyer@agilex.com>
2013-06-21 15:07:19 -0400

    testMaxViewers, testGenerateAllPossibleElimination, and powerSet.

--- DemoProj/src/test/java/Group/DemoProj/AlgsTest.java
+++ DemoProj/src/test/java/Group/DemoProj/AlgsTest.java
@@ -3,20 +3,18 @@ package Group.DemoProj;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.After;
-import org.junit.Before;
 
 import junit.framework.TestCase;
 
 public class AlgsTest extends TestCase
 {
-	@Before
+
 	public void setUp(){
-		emptyList = new java.util.ArrayList();
+		//emptyList = new java.util.ArrayList();
 	}
 	
-	@After public void tearDown(){
-		emptyList = null;
+	 public void tearDown(){
+	//	emptyList = null;
 	}
 
 	public void testPowerSet()
@@ -25,32 +23,30 @@ public class AlgsTest extends TestCase
 		 mySet.add(1);
 		 mySet.add(2);
 		 mySet.add(3);
-		 for (Set<Integer> s : Algs.powerSet(mySet)) {
-		     System.out.println(s);
-		 }
-		
-		assertTrue(mySet.contains(1));
-		assertTrue(mySet.contains(2));
-		assertTrue(mySet.contains(3));
-		assertFalse(mySet.contains(0));
+		 Set<Set<Integer>> pSet = Algs.powerSet(mySet);
+		 
+		assertTrue(pSet.contains(mySet));
+		assertTrue(pSet.size() == Math.pow(2, mySet.size()));
 	}
 	
 	public void testMaxViewers()
 	{
+		Viewer viewer1 = new Viewer();
 		Show show1 = new Show();
 		show1.setnCats(2);
 		show1.setnDogs(3);
 		show1.setnViewers(10);
 		Algs.maxViewers(show1);
-		assertTrue(show1.getViewers().contains(10));
+		assertTrue(show1.getnViewers() == 10);
 	}
 	
 	public void testGenerateAllPossibleEliminations()
 	{
 		Show show1 = new Show();
-		show1.setnCats(5);
-		show1.setnDogs(13);
-		show1.setnViewers(20);
-		Algs.generateAllPossibleEliminations(show1);
+		show1.setnCats(1);
+		show1.setnDogs(2);
+		show1.setnViewers(3);
+		Set<Set<String>> allPos = Algs.generateAllPossibleEliminations(show1);
+		assertTrue(allPos.size() == Math.pow(2, show1.getnCats() + show1.getnDogs()));
 	}
 }
